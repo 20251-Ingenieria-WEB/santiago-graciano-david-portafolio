0b69765d05fa06aa034a36c8ccdff5e2c2bbdace
Santiago Graciano <santiago.flowing@gmail.com>
2025-05-25 22:28:04 -0500

    carpeta app

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // indica a Next que use `src/app` como carpeta de App Router
-  experimental: {
-    appDir: true,
-  },
-  // aquí puedes añadir más opciones si las necesitas
+  /* config options here */
 };
 
 export default nextConfig;
